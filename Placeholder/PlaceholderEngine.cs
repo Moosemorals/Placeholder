f545fed52e2998f54bb6db8cbad2418b7bb6d9ac
Osric Wilkinson <gitmoose@fluffypeople.com>
2019-09-28 15:57:15 +0100

    Basically working

---
+++ Placeholder/PlaceholderEngine.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OcelotPlaceholders {
+
+    /// <summary>
+    /// Manage placeholders. Call 'Register' to add new placeholder types, then
+    /// call'Transform' with the string to parse.
+    /// </summary>
+    public class PlaceholderEngine {
+
+        private readonly IDictionary<string, Func<string, string>> Operators = new Dictionary<string, Func<string, string>>();
+
+        /// <summary>
+        /// Create a new PlaceholderEngine. "bold" and "link" placeholders are already handled.
+        /// </summary>
+        public PlaceholderEngine() {
+            Register("bold", ParseBold);
+            Register("link", ParseLink);
+        }
+
+        /// <summary>
+        /// Register a placeholder handler
+        /// </summary>
+        /// <param name="name">The name of the placeholder</param>
+        /// <param name="func">The handler. Function that takes a string and returns a string, or null if it can't handle the input.</param>
+        public void Register(string name, Func<string, string> func) {
+            Operators.Add(name.ToLower(), func);
+        }
+
+        /// <summary>
+        /// Transform the placeholders in input based on currently registered handlers.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public string Transform(string input) {
+            string result = input;
+            int offset = 0;
+
+            Stack<int> starts = new Stack<int>();
+
+            while (offset < result.Length) {
+                char c = result[offset];
+
+                if (c == '[') {
+                    starts.Push(offset);
+                } else if (c == ']') {
+                    if (starts.Count > 0) {
+                        int start = starts.Pop();
+                        string text = result.Substring(start + 1, offset - start - 1);
+                        string candidate = Parse(text);
+                        if (candidate != null) {
+                            result = result.Remove(start, offset - start + 1).Insert(start, candidate);
+                            offset = start;
+                        }
+                    }
+                }
+                offset++;
+            }
+
+            return result;
+        }
+
+        private string Parse(string text) {
+            int colon = text.IndexOf(':');
+            if (colon == -1) {
+                return null;
+            }
+
+            string type = text.Substring(0, colon).ToLower();
+            string args = text.Substring(colon + 1);
+            if (Operators.ContainsKey(type)) {
+                return Operators[type].Invoke(args);
+            }
+
+            return null;
+        }
+
+        private string ParseBold(string args) {
+            return "<strong>" + args +"</strong>";
+        }
+
+        private string ParseLink(string args) {
+            string[] parts = args.Split(new char[] { ':' }, 2);
+
+            return "<a href='" + parts[1] + "'>" + parts[0] + "</a>";
+        }
+    }
+}
---
+++ PlaceholderTests/SimpleTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OcelotPlaceholders;
+
+namespace PlaceholderTests {
+    [TestClass]
+    public class SimpleTests {
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "Placeholders.csv", "Placeholders#csv", DataAccessMethod.Sequential)]
+        public void Basics() {
+
+            string source = Convert.ToString(TestContext.DataRow["source"]);
+            string expected = Convert.ToString(TestContext.DataRow["expected"]);
+            string description = Convert.ToString(TestContext.DataRow["description"]);
+
+            PlaceholderEngine engine = new PlaceholderEngine();
+
+            Assert.AreEqual(expected, engine.Transform(source), description);
+        }
+
+        [TestMethod]
+        public void Register() {
+            PlaceholderEngine engine = new PlaceholderEngine();
+
+            engine.Register("double", d => d + d);
+
+            Assert.AreEqual("aa", engine.Transform("[double:a]"));
+        }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "Labels.csv", "Labels#csv", DataAccessMethod.Sequential)]
+        public void Labels() {
+            IDictionary<string, string> labels = new Dictionary<string, string> {
+                { "alpha", "Hello" },
+                { "beta", "World" },
+                { "Hello", "Nested" },
+                { "Hello World", "Now that's just silly" },
+                { "Hello:World", "Now that's just silly" },
+            };
+
+            PlaceholderEngine engine = new PlaceholderEngine();
+
+            engine.Register("label", arg => labels.ContainsKey(arg) ? labels[arg] : null);
+            string source = Convert.ToString(TestContext.DataRow["source"]);
+            string expected = Convert.ToString(TestContext.DataRow["expected"]);
+
+            Assert.AreEqual(expected, engine.Transform(source));
+        }
+
+        private TestContext testContextInstance;
+        public TestContext TestContext {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+    }
+
+
+}
