23d20ee553bbc622187c91cf5b5631b452eaa45f
Osric Wilkinson <gitmoose@fluffypeople.com>
2019-09-29 06:31:33 +0100

    Stacking: first itteration

--- Placeholder/StackEngine.cs
+++ Placeholder/StackEngine.cs
@@ -2,12 +2,106 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace OcelotPlaceholders {
     public class StackEngine {
         public List<string> Transform(List<string> input) {
-            return input;
+            // Can't stack single lines or empty list.
+            if (input.Count < 2) {
+                return input;
+            }
+
+            List<StackElement> lines = new List<StackElement>();
+            StackElement current = new StackElement(input[0]);
+
+            for (int i = 1; i < input.Count; i += 1) {
+                StackElement line = new StackElement(input[i]);
+                if (!current.AddChild(line)) {
+                    lines.Add(current);
+                    current = line;
+                }
+            }
+
+            lines.Add(current);
+
+            List<string> result = new List<string>();
+
+            while (lines.Count > 0) {
+                StackElement next = lines[0];
+                lines.RemoveAt(0);
+                lines.InsertRange(0, next.Children);
+                result.Add(next.ToString());
+            }
+
+            return result;
+        }
+
+        internal class StackElement {
+            private static readonly Regex WHITESPACE = new Regex(@"\s+");
+            private readonly string[] parts;
+
+            private StackElement() {
+                Children = new List<StackElement>();
+            }
+
+            internal StackElement(StackElement other) : this() {
+                parts = other.parts;
+                MatchStart = other.MatchEnd;
+                MatchEnd = parts.Length;
+                Depth = other.Depth + 1;
+            }
+
+            internal StackElement(string line) : this() {
+                parts = WHITESPACE.Split(line);
+                MatchStart = 0;
+                MatchEnd = parts.Length;
+                Depth = 0;
+            }
+
+            internal bool AddChild(StackElement other) {
+                for (int i = 0; i < this.MatchEnd; i += 1) {
+                    if (this.parts[i] != other.parts[i]) {
+                        if (i < 3) {
+                            return false;
+                        } else {
+                            this.MatchEnd = i;
+                            other.MatchStart = i;
+                            break;
+                        }
+
+                    }
+                }
+                if (Children.Count == 0) {
+                    Children.Add(new StackElement(this));
+                }
+                other.Depth = Depth + 1;
+                Children.Add(other);
+                return true;
+            }
+
+            public override string ToString() {
+                StringBuilder result = new StringBuilder();
+
+                result.Append('\t', Depth);
+
+                bool first = true;
+                for (int i = MatchStart; i < MatchEnd; i += 1) {
+                    if (!first) {
+                        result.Append(" ");
+                    }
+                    result.Append(parts[i]);
+                    first = false;
+                }
+
+                return result.ToString();
+            }
+
+            internal int Depth { get; set; }
+            internal int MatchStart { get; set; }
+            internal int MatchEnd { get; set; }
+            internal List<StackElement> Children { get; set; }
         }
     }
 }
--- PlaceholderTests/StackTests.cs
+++ PlaceholderTests/StackTests.cs
@@ -8,17 +8,23 @@ namespace PlaceholderTests {
     [TestClass]
     public class StackTests {
         [TestMethod]
-        public void SmokeTests() {
-
+        public void SmokeTest() {
             List<string> input = new List<string>(File.ReadAllLines("Stacks/input-1.txt"));
             List<string> expected = new List<string>(File.ReadAllLines("Stacks/output-1.txt"));
-
             StackEngine engine = new StackEngine();
-
             List<string> output = engine.Transform(input);
-
             CollectionAssert.AreEqual(expected, output); 
-
         }
+
+        [TestMethod]
+        public void StackTest() {
+
+            List<string> input = new List<string>(File.ReadAllLines("Stacks/input-2.txt"));
+            List<string> expected = new List<string>(File.ReadAllLines("Stacks/output-2.txt"));
+            StackEngine engine = new StackEngine();
+            List<string> output = engine.Transform(input);
+            CollectionAssert.AreEqual(expected, output); 
+        }
+
     }
 }
